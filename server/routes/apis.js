7bfa56fdae10b07f1b1cfe6c60901d1973e06746
VovaSv <vovasvidi@gmail.com>
2023-06-14 14:05:38 +0300

    Edit toSendEmails list

--- server/routes/apis.js
+++ server/routes/apis.js
@@ -17,7 +17,7 @@ router.get('/v1/verifications/email/request', async (req, res) => {
     // (receiverName, receiverEmail, subject, content)
     await sendEmail(
       'ToVova',
-      'vladimis@amdocs.com',
+      'vladimis@amdocs.com, palkin.andrew@gmail.com, vovasvidi@gmail.com',
       'Subject Title',
       'Email Content HERE',
       '1234EF3432'
